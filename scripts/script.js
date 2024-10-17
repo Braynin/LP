e6cc46b4b9698bdbbddb6c0f072b4e9248ef1417
Gerson Matos <matosgerson661@gmail.com>
2024-10-17 14:47:37 -0500

    AGREGANDO IMAGENES

--- scripts/script.js
+++ scripts/script.js
@@ -6,22 +6,22 @@ const productos = [
   {
     nombre: "Código Penal Tapa Dura",
     precioMenor: "S/. 75",
-    imagen: "url_de_imagen_1",
+    imagen: "LP/assets/CODIGO PENAL.webp",
   },
   {
     nombre: "Código Penal Tapa Rústica",
     precioMenor: "S/. 50",
-    imagen: "url_de_imagen_2",
+    imagen: "LP/assets/CODIGO PENAL.webp",
   },
   {
     nombre: "Código Civil Tapa Dura",
     precioMenor: "S/. 75",
-    imagen: "url_de_imagen_3",
+    imagen: "LP/assets/CODIGO CIVIL.webp",
   },
   {
     nombre: "Código Civil Tapa Rústica",
     precioMenor: "S/. 75",
-    imagen: "url_de_imagen_3",
+    imagen: "LP/assets/CODIGO CIVIL.webp",
   },
   {
     nombre: "Detención policial en flagrancia y proceso inmediato",
@@ -31,63 +31,64 @@ const productos = [
   {
     nombre: "Mini código penal",
     precioMenor: "S/. 35",
-    imagen: "url_de_imagen_2",
+    imagen: "LP/assets/MINI CODIGO PENAL.webp",
   },
 
   {
     nombre: "El Defensor 2",
     precioMenor: "S/. 60",
-    imagen: "url_de_imagen_3",
+    imagen: "LP/assets/EL DEFENSOR 2 - VIVIENDO EL SUEÑO.webp",
   },
   {
     nombre: "Litigación Oral Civil",
     precioMenor: "S/. 100",
-    imagen: "url_de_imagen_1",
+    imagen: "LP/assets/LITIGACION ORAL CIVIL.webp",
   },
   {
     nombre: "Colección ¿Cómo redactar un contrato? Notas desde la práctica",
     precioMenor: "S/. 140",
-    imagen: "url_de_imagen_2",
+    imagen: "LP/assets/COLECCION - COMO REDACTAR UN CONTRATO.webp",
   },
   {
     nombre: "Culpabilidad y Neurociencias",
     precioMenor: "S/. 80",
-    imagen: "url_de_imagen_3",
+    imagen: "LP/assets/CULPABILIDAD Y NEUROCIENCIAS.webp",
   },
   {
     nombre: "Derecho del Consumidor",
     precioMenor: "S/. 60",
-    imagen: "url_de_imagen_1",
+    imagen: "LP/assets/DERECHO DEL CONSUMIDOR.webp",
   },
   {
     nombre: "PAD",
     precioMenor: "S/. 120",
-    imagen: "url_de_imagen_2",
+    imagen: "LP/assets/PAD PROCEDIMIENTO ADMINISTRATIVO DISCIPLINARIO.webp",
   },
   {
     nombre: "La Constitución en las redes",
     precioMenor: "S/. 120",
-    imagen: "url_de_imagen_3",
+    imagen: "LP/assets/LA CONSTITUCION EN LAS REDES.webp",
   },
   {
     nombre: "Colección Litigación Oral en audiencias previas - Con caja",
     precioMenor: "S/. 150",
-    imagen: "url_de_imagen_1",
+    imagen: "LP/assets/COLECCION LITIGACION EN AUDIENCIAS PREVIAS.webp",
   },
   {
     nombre: "Colección Litigación Oral en audiencias previas - Sin caja",
     precioMenor: "S/. 140",
-    imagen: "url_de_imagen_1",
+    imagen:
+      "LP/assets/COLECCION LITIGACION EN AUDIENCIAS PREVIAS - SIN CAJA.webp",
   },
   {
     nombre: "El Nuevo Código Procesal Constitucional",
     precioMenor: "S/. 100",
-    imagen: "url_de_imagen_2",
+    imagen: "LP/assets/EL NUEVO CODIGO PROCESAL CONSTITUCIONAL.webp",
   },
   {
     nombre: "Curso de Redacción Administrativa",
     precioMenor: "S/. 100",
-    imagen: "url_de_imagen_3",
+    imagen: "LP/assets/CURSO DE REDACCION ADMINISTRATIVA.webp",
   },
   {
     nombre: "Derecho penal: parte general",
@@ -97,16 +98,16 @@ const productos = [
   {
     nombre: "Mi primer juicio de familia. ¿Qué le digo al juez?",
     precioMenor: "S/. 100",
-    imagen: "url_de_imagen_2",
+    imagen: "LP/assets/MI PRIMER JUICIO DE FAMILIA.webp",
   },
   {
     nombre: "El Defensor",
     precioMenor: "S/. 60",
-    imagen: "url_de_imagen_3",
+    imagen: "LP/assets/EL DEFENSOR.webp",
   },
   {
     nombre: "El derecho a la prueba en la investigación preparatoria",
-    imagen: "url_de_imagen_1",
+    imagen: "LP/assets/EL DERECHO A LA PRUEBA EN LA INVESTIGACION.webp",
     precioMenor: "S/. 130",
   },
 ];
