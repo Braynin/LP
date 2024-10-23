89c682bad8954e462cefa944591845d06aad8c5f
Gerson Matos <matosgerson661@gmail.com>
2024-10-23 10:14:20 -0500

    NUEVO JS DE PRODUCTOS

--- scripts/productos.js
+++ scripts/productos.js
@@ -1,3 +1,4 @@
+//TARJETA DE PRODUCTOS
 class Productos {
   constructor(id, nombre, precioMenor, precioMayor, imagen, igv) {
     this.id = id;
@@ -20,6 +21,7 @@ const producto1 = new Productos(
   "1",
   "Código Penal Tapa Dura",
   "S/. 75",
+  "",
   ["assets/CODIGO PENAL.png"],
   "Incluye IGV"
 );
@@ -27,6 +29,7 @@ const producto2 = new Productos(
   "2",
   "Código Penal Tapa Rústica",
   "S/. 50",
+  "",
   ["assets/CP_TR3-removebg-preview.png"],
   "Incluye IGV"
 );
@@ -34,6 +37,7 @@ const producto3 = new Productos(
   "3",
   "Código Civil Tapa Dura",
   "S/. 75",
+  "",
   ["assets/CODIGO CIVIL.webp"],
   "Incluye IGV"
 );
@@ -41,6 +45,7 @@ const producto4 = new Productos(
   "4",
   "Código Civil Tapa Rústica",
   "S/. 75",
+  "",
   ["assets/CC_TR_3-removebg-preview.png"],
   "Incluye IGV"
 );
@@ -48,6 +53,7 @@ const producto5 = new Productos(
   "5",
   "Detención policial en flagrancia y proceso inmediato",
   "S/. 130",
+  "",
   ["url_de_imagen_1"],
   "Incluye IGV"
 );
@@ -55,6 +61,7 @@ const producto6 = new Productos(
   "6",
   "Mini código penal",
   "S/. 35",
+  "",
   ["assets/MINI CODIGO PENAL.webp"],
   "Incluye IGV"
 );
@@ -62,6 +69,7 @@ const producto7 = new Productos(
   "7",
   "El Defensor 2",
   "S/. 60",
+  "",
   ["assets/EL DEFENSOR 2 - VIVIENDO EL SUEÑO.webp"],
   "Incluye IGV"
 );
@@ -69,6 +77,7 @@ const producto8 = new Productos(
   "8",
   "Litigación Oral Civil",
   "S/. 100",
+  "",
   ["assets/LITIGACION ORAL CIVIL.webp"],
   "Incluye IGV"
 );
@@ -76,6 +85,7 @@ const producto9 = new Productos(
   "9",
   "Colección ¿Cómo redactar un contrato? Notas desde la práctica",
   "S/. 140",
+  "",
   ["assets/COLECCION - COMO REDACTAR UN CONTRATO.webp"],
   "Incluye IGV"
 );
@@ -83,6 +93,7 @@ const producto10 = new Productos(
   "10",
   "Culpabilidad y Neurociencias",
   "S/. 80",
+  "",
   ["assets/CULPABILIDAD Y NEUROCIENCIAS.webp"],
   "Incluye IGV"
 );
@@ -90,6 +101,7 @@ const producto11 = new Productos(
   "11",
   "Derecho del Consumidor",
   "S/. 60",
+  "",
   ["assets/DERECHO DEL CONSUMIDOR.webp"],
   "Incluye IGV"
 );
@@ -97,6 +109,7 @@ const producto12 = new Productos(
   "12",
   "PAD",
   "S/. 120",
+  "",
   ["assets/PAD PROCEDIMIENTO ADMINISTRATIVO DISCIPLINARIO.webp"],
   "Incluye IGV"
 );
@@ -104,6 +117,7 @@ const producto13 = new Productos(
   "13",
   "La Constitución en las redes",
   "S/. 120",
+  "",
   ["assets/LA CONSTITUCION EN LAS REDES.webp"],
   "Incluye IGV"
 );
@@ -111,6 +125,7 @@ const producto14 = new Productos(
   "14",
   "Colección Litigación Oral en audiencias previas - Con caja",
   "S/. 150",
+  "",
   ["assets/COLECCION LITIGACION EN AUDIENCIAS PREVIAS.webp"],
   "Incluye IGV"
 );
@@ -118,6 +133,7 @@ const producto15 = new Productos(
   "15",
   "Colección Litigación Oral en audiencias previas - Sin caja",
   "S/. 140",
+  "",
   ["assets/COLECCION LITIGACION EN AUDIENCIAS PREVIAS - SIN CAJA.webp"],
   "Incluye IGV"
 );
@@ -125,6 +141,7 @@ const producto16 = new Productos(
   "16",
   "El Nuevo Código Procesal Constitucional",
   "S/. 100",
+  "",
   ["assets/EL NUEVO CODIGO PROCESAL CONSTITUCIONAL.webp"],
   "Incluye IGV"
 );
@@ -132,6 +149,7 @@ const producto17 = new Productos(
   "17",
   "Curso de Redacción Administrativa",
   "S/. 100",
+  "",
   ["assets/CURSO DE REDACCION ADMINISTRATIVA.webp"],
   "Incluye IGV"
 );
@@ -139,6 +157,7 @@ const producto18 = new Productos(
   "18",
   "Derecho penal: parte general",
   "S/. 130",
+  "",
   ["url_de_imagen_1"],
   "Incluye IGV"
 );
@@ -146,6 +165,7 @@ const producto19 = new Productos(
   "19",
   "Mi primer juicio de familia. ¿Qué le digo al juez?",
   "S/. 100",
+  "",
   ["assets/MI PRIMER JUICIO DE FAMILIA.webp"],
   "Incluye IGV"
 );
@@ -153,18 +173,21 @@ const producto20 = new Productos(
   "20",
   "El Defensor",
   "S/. 60",
+  "",
   ["assets/EL DEFENSOR.png"],
   "Incluye IGV"
 );
 const producto21 = new Productos(
   "21",
   "El derecho a la prueba en la investigación preparatoria",
-  ["assets/EL DERECHO A LA PRUEBA EN LA INVESTIGACION.webp"],
   "S/. 130",
+  "",
+  ["assets/EL DERECHO A LA PRUEBA EN LA INVESTIGACION.webp"],
+
   "Incluye IGV"
 );
 
-const productos = [
+const arrayProductos = [
   producto1,
   producto2,
   producto3,
@@ -190,8 +213,8 @@ const productos = [
 
 function createCard(producto) {
   return `
-  <a class = "product-card" href = "./details.html?id=${producto.id}"   
-  <img class="product-img" src="${producto.imagen}" alt="${producto.nombre}" />
+  <a class = "product-card" href = "./details.html?id=${producto.id}" >  
+  <img class="product-img" src="${producto.imagen[0]}" alt="${producto.nombre}" />
       <div class="product-info">
         <span class="product-title">${producto.nombre}</span>
         <div class="product-price-block">
@@ -214,3 +237,7 @@ function printCards(arrayProductos, selector) {
   const productosContainer = document.getElementById(selector);
   productosContainer.innerHTML = productosTemplate;
 }
+
+printCards(arrayProductos, "products");
+
+//TARJETA DE ORFERTAS
