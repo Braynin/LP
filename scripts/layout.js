940aa6a3f04d2cb2e6599b089c4e3b37df670ffd
Braynin <brayhannb@gmail.com>
2024-10-23 17:00:31 -0500

    avance de layout

--- scripts/layout.js
+++ scripts/layout.js
@@ -29,6 +29,7 @@ const message3 = new message("llamanos o escribenos", "#");
 
 const arrayMessages = [message1, message2, message3];
 
+/*Functions */
 function createMessage(message) {
   return `
 <a class="message" href="${message.linkto}">${message.text}</a>
@@ -41,16 +42,23 @@ function createOption(option) {
 `;
 }
 
-function printer(array) {
+function template(array, creater) {
   let template = "";
-  array.forEach((option) => {
-    template += createOption(option);
+  array.forEach((item) => {
+    template += creater(item);
   });
   return template;
 }
-`<nav class="nav">
+
+function printe(array, creater) {
+  return;
+  `<nav class="nav">
           <ul class="nav-options">
-            ${optionsTemplate}
+            ${template(array, creater)}
           </ul>
         </nav>`;
-navContainer.innerHTML = printOptions(arrayOptions);
+}
+
+/*Printers */
+navContainer.innerHTML = printer(arrayOptions, createOption);
+navContainer.innerHTML = printer(arrayMessages, createMessage);
