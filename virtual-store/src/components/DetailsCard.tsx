67feb443a3827c79d9c63e9021d79d6f65226f67
Gerson Matos <matosgerson661@gmail.com>
2024-11-22 14:10:10 -0500

    nuevos cambios aqui

--- virtual-store/src/components/DetailsCard.tsx
+++ virtual-store/src/components/DetailsCard.tsx
@@ -67,122 +67,131 @@ function DetailsCard() {
 
   return (
     <div className={styles["details-container"]}>
-      <section className={styles["product-images-block"]}>
-        <div className={styles["product-images"]}>
-          {renderMiniImages(product)}
+      <section className={styles["product-images-checkout-block"]}>
+        <div className={styles["product-images-block"]}>
+          <div className={styles["product-images"]}>
+            {renderMiniImages(product)}
+          </div>
+
+          <div
+            className={`${styles["big-img-container"]} ${
+              isZoomed ? styles["zoomed"] : ""
+            }`}
+            onMouseEnter={() => setIsZoomed(true)}
+            onMouseLeave={() => setIsZoomed(false)}
+            onMouseMove={handleMouseMove}
+          >
+            <img
+              className={styles["big-img"]}
+              id="big-img"
+              src={product.imagen[0]}
+              alt={product.nombre}
+              style={
+                isZoomed
+                  ? {
+                      transform: `scale(2) translate(-${zoomPosition.x}%, -${zoomPosition.y}%)`,
+                      transformOrigin: `${zoomPosition.x}% ${zoomPosition.y}%`,
+                    }
+                  : undefined
+              }
+            />
+          </div>
         </div>
 
-        <div
-          className={`${styles["big-img-container"]} ${
-            isZoomed ? styles["zoomed"] : ""
-          }`}
-          onMouseEnter={() => setIsZoomed(true)}
-          onMouseLeave={() => setIsZoomed(false)}
-          onMouseMove={handleMouseMove}
-        >
-          <img
-            className={styles["big-img"]}
-            id="big-img"
-            src={product.imagen[0]}
-            alt={product.nombre}
-            style={
-              isZoomed
-                ? {
-                    transform: `scale(2) translate(-${zoomPosition.x}%, -${zoomPosition.y}%)`,
-                    transformOrigin: `${zoomPosition.x}% ${zoomPosition.y}%`, // Centra el zoom según el cursor
-                  }
-                : undefined
-            }
-          />
-        </div>
-      </section>
-      <div className={styles["product-checkout-block"]}>
-        <div className={styles["checkout-container"]}>
-          <span className={styles["checkout-total-label"]}>Total:</span>
-          <h2 id="price" className={styles["checkout-total-price"]}>
-            S/.{price}
-          </h2>
-          <p className={styles["checkout-description"]}></p>
-          <ul className={styles["checkout-policy-list"]}>
-            <li>
-              <a className={styles["policy-icon"]} href="#">
-                <img
-                  className={styles["img-icon"]}
-                  src="../public/iconPDF.webp"
-                  alt="Plane"
+        <div className={styles["product-checkout-block"]}>
+          <div className={styles["checkout-container"]}>
+            <span className={styles["checkout-total-label"]}>Total:</span>
+            <h2 id="price" className={styles["checkout-total-price"]}>
+              S/.{price}
+            </h2>
+            <ul className={styles["checkout-policy-list"]}>
+              <li>
+                <a className={styles["policy-icon"]} href="#">
+                  <img
+                    className={styles["img-icon"]}
+                    src="../public/iconPDF.webp"
+                    alt="Plane"
+                  />
+                </a>
+                <span className={styles["policy-desc"]}>
+                  Descarga el índice
+                </span>
+              </li>
+              <li>
+                <a className={styles["policy-icon"]} href="#">
+                  <img
+                    className={styles["img-icon"]}
+                    src="../public/delivery.webp"
+                    alt="Plane"
+                  />
+                </a>
+                <span className={styles["policy-desc"]}>
+                  Recibe aproximadamente entre 3 a 5 días hábiles tu pedido.
+                </span>
+              </li>
+            </ul>
+            <div className={styles["checkout-process"]}>
+              <div className={styles["top"]}>
+                <input
+                  id="quantity"
+                  type="number"
+                  min="1"
+                  value={quantity}
+                  onChange={changePrice}
                 />
-              </a>
-
-              <span className={styles["policy-desc"]}>Descarga el índice</span>
-            </li>
-            <li>
-              <a className={styles["policy-icon"]} href="#">
-                <img
-                  className={styles["img-icon"]}
-                  src="../public/delivery.webp"
-                  alt="Plane"
-                />
-              </a>
-              <span className={styles["policy-desc"]}>
-                Recibe aproximadamente entre 3 a 5 días hábiles tu pedido.
-              </span>
-            </li>
-          </ul>
-          <div className={styles["checkout-process"]}>
-            <div className={styles["top"]}>
-              <input
-                id="quantity"
-                type="number"
-                min="1"
-                value={quantity}
-                onChange={changePrice}
-              />
-              <a href="https://wa.me/939613209" className={styles["cart-btn"]}>
-                Compra aquí
-              </a>
+                <a
+                  href="https://wa.me/939613209"
+                  className={styles["cart-btn"]}
+                >
+                  Compra aquí
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
 
-      <div className="product-sipnosis-block">
-        <h2>Sipnosis:</h2>
-        <span className="product-sipnosis">{product.sipnosis}</span>
-      </div>
-      <div className={styles["product-information-block"]}>
-        <ul className={styles["product-title"]}>
-          <li>Detalles de producto:</li>
-        </ul>
-        <ul className={styles["product-details"]}>
-          <li className={styles["product-description"]}>
-            <strong>ISBN: </strong> {product.isbn}
-          </li>
-          <li className={styles["product-description"]}>
-            <strong>Autor: </strong> {product.autor}
-          </li>
-          <li className={styles["product-description"]}>
-            <strong>Editorial: </strong> {product.editorial}
-          </li>
-          <li className={styles["product-description"]}>
-            <strong>Año: </strong> {product.año}
-          </li>
-          <li className={styles["product-description"]}>
-            <strong>Páginas: </strong> {product.paginas}
-          </li>
-          <li className={styles["product-description"]}>
-            <strong>Presentación: </strong> {product.presentacion}
-          </li>
-          <li className={styles["product-description"]}>
-            <strong>Peso: </strong> {product.peso}
-          </li>
-          <li className={styles["product-description"]}>
-            <strong>Alto: </strong> {product.alto}
-          </li>
-          <li className={styles["product-description"]}>
-            <strong>Ancho: </strong> {product.ancho}
-          </li>
-        </ul>
-      </div>
+      <section className={styles["container-sipnosis-information"]}>
+        <div className={styles["product-sipnosis-block"]}>
+          <h2 className={styles["product-title"]}>Sipnosis:</h2>
+          <span className={styles["product-sipnosis"]}>{product.sipnosis}</span>
+        </div>
+
+        <div className={styles["product-information-block"]}>
+          <ul>
+            <li className={styles["product-title"]}>Detalles de producto:</li>
+          </ul>
+          <ul className={styles["product-details"]}>
+            <li className={styles["product-description"]}>
+              <strong>ISBN: </strong> {product.isbn}
+            </li>
+            <li className={styles["product-description"]}>
+              <strong>Autor: </strong> {product.autor}
+            </li>
+            <li className={styles["product-description"]}>
+              <strong>Editorial: </strong> {product.editorial}
+            </li>
+            <li className={styles["product-description"]}>
+              <strong>Año: </strong> {product.año}
+            </li>
+            <li className={styles["product-description"]}>
+              <strong>Páginas: </strong> {product.paginas}
+            </li>
+            <li className={styles["product-description"]}>
+              <strong>Presentación: </strong> {product.presentacion}
+            </li>
+            <li className={styles["product-description"]}>
+              <strong>Peso: </strong> {product.peso}
+            </li>
+            <li className={styles["product-description"]}>
+              <strong>Alto: </strong> {product.alto}
+            </li>
+            <li className={styles["product-description"]}>
+              <strong>Ancho: </strong> {product.ancho}
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 }
