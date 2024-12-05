2ae9e738717de251e620eb9100d687476d984381
“GersonMatos” <matosgerson661@gmail.com>
2024-12-05 17:22:26 -0500

    nuevos cambios

--- virtual-store/src/components/Header.tsx
+++ virtual-store/src/components/Header.tsx
@@ -2,14 +2,14 @@
 
 import Messages from "./Messages.tsx";
 import HeaderSearch from "./HeaderSearch.tsx";
-import NavBar from "./NavBar.tsx";
+
 
 function Header() {
   return (
     <>
       <Messages />
       <HeaderSearch />
-      {/* <NavBar /> */}
+      
     </>
   );
 }
